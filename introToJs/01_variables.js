15e090a4e2185d0bafb2df09cc1a84cebd07f27a
M.Zain ul abideen <140058199+Zain308@users.noreply.github.com>
2025-11-28 14:02:31 +0000

    Conversions OF variables are done

---
+++ introToJs/01_variables.js
@@ -0,0 +1,5 @@
+let a = 10
+var b = "hello"
+const c = 3.14
+
+// there is how we initialise variables
---
+++ introToJs/03_conversioVariables.js
@@ -0,0 +1,38 @@
+// let score = 33
+// console.log(typeof score)
+
+// let score = "33"
+// console.log(typeof score)
+
+// let score = "33"
+// let StringInNumber = Number(score)
+// console.log(typeof StringInNumber)
+// console.log(StringInNumber)
+
+
+// let score = "33ass"
+// let StringInNumber = Number(score)
+// console.log(typeof StringInNumber)
+// console.log(StringInNumber)
+
+
+let score = true
+let StringInNumber = Number(score)
+console.log(typeof StringInNumber)
+console.log(StringInNumber)
+
+// ****************Operations***************
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+
+
+console.log("1" + 2 ) //12
+console.log(1 + "2" ) //12
+console.log(1 +2 + "2" ) //32
+console.log("1" + 2  + 2 ) //122 because if the string is first then it follow the string sequence along the line
+
+
+
